cf1f7c57a8b33738c69cf1d8302937a20db73a70
pigeonflight <david@alteroo.com>
2015-06-01 02:20:17 +0000

    refactored example, it shoud now loop through each repo and return the number of commits

--- examples/lab1.py
+++ examples/lab1.py
@@ -1,14 +1,14 @@
 from gradinator import Gradinator
 import csv
 import os
+token = os.getenv('GIT_TOKEN')
+# generate token at https://github.com/settings/tokens
+
 with open('lab1.csv', 'rb') as csvfile:
     submissions = csv.reader(csvfile, delimiter=',',quotechar='"')
     for submission in submissions:
         fullrepourl = submission[4].strip()
- 
 
-token = os.getenv('GIT_TOKEN')
-# generate token at https://github.com/settings/tokens
-g = Gradinator(fullrepourl=fullrepourl,token=token)
-g.clone_repo()
-print "{} commits".format(g.commit_count())
+    g = Gradinator(fullrepourl=fullrepourl,token=token)
+    g.clone_repo()
+    print "{} commits".format(g.commit_count())
