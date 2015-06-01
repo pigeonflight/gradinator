3f92d73936d10f3493a40139b50bc46f9754ed10
David Bain <pigeonflight@gmail.com>
2015-05-31 20:19:32 -0500

    Update lab1.py

--- examples/lab1.py
+++ examples/lab1.py
@@ -1,6 +1,6 @@
 username=pigeonflight
 repo=gradinator
-token = "XXXXXXXXX"
+token = "XXXXXXXXX" # generate token at https://github.com/pigeonflight/gradinator
 g = Gradinator(username,repo,token=token)
 g.clone_repo()
 print "{} commits".format(g.commit_count())
