8e05faf18db477fedf00982961ebe63c5955f7b6
pigeonflight <david@alteroo.com>
2015-06-01 01:48:02 +0000

    updated lab1.py example

--- examples/lab1.py
+++ examples/lab1.py
@@ -1,5 +1,6 @@
-#from gradinator import Gradinator
+from gradinator import Gradinator
 import csv
+import os
 with open('lab1.csv', 'rb') as csvfile:
     submissions = csv.reader(csvfile)
     for submission in submissions:
@@ -7,7 +8,8 @@ with open('lab1.csv', 'rb') as csvfile:
 
 #username=pigeonflight
 #repo=gradinator
-token = "XXXXXXXXX" # generate token at https://github.com/settings/tokens
+#token = "XXXXXXXXX" # generate token at https://github.com/settings/tokens
+token = os.getenv('GIT_TOKEN')
 g = Gradinator(fullrepourl=fullrepourl,token=token)
 g.clone_repo()
 print "{} commits".format(g.commit_count())
